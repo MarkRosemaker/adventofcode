24220cde50a974a06aac0a25007facc951d93699
Mark Rösler <marcodroesler@gmail.com>
2021-12-06 18:09:44 +0100

    changes from last year

--- 2020/internal/TEMPLATE/data/data.go
+++ 2020/internal/TEMPLATE/data/data.go
@@ -1,1 +1,9 @@
 package data
+
+var Example = []Struct{
+	{Low: 1, High: 3, Letter: 'a', Password: "abcde"},
+	{Low: 1, High: 3, Letter: 'b', Password: "cdefg"},
+	{Low: 2, High: 9, Letter: 'c', Password: "ccccccccc"},
+}
+
+var ExampleResultOne = []bool{true, false, true}
